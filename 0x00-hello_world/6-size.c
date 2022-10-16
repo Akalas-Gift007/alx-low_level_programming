50f437c7f8c69a9ed8952fdddd8ce0f59db6f60f
Akalas-Gift007 <giftakalagboro@gmail.com>
2022-10-16 14:49:35 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ * @void: parameter
+ * Return: 0(success)
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+       printf("size of an int: %d byte(s)\n", sizeof(int));
+       printf("size of a lon int: %d byte(s)\n", sizeof(long int));
+       printf("size of a long long int: %d byte(s)\n", sizeof(long long int));     
+     return(0);  
+}
