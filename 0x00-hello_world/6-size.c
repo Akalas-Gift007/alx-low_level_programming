ecd12ce2ceca9b3fac163d5df92302ffdbe3225c
Akalas-Gift007 <giftakalagboro@gmail.com>
2022-10-16 15:03:55 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	printfi("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of a int: %d byte(s)\n", sizeof(int));
        printf("size of an long int: %d byte(s)\n", sizeof(long int));
        printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
