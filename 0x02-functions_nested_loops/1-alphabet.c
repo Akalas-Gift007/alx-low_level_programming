d0db722a12a8c7b36f218eee5c185792f824a582
Akalas-Gift007 <giftakalagboro@gmail.com>
2022-10-29 17:54:59 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#nclude "main.h"
+
+/**
+ * print alphabet -prints lowercase alpha
+ */
+void print_alphabet(void);
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+		_putchar(lc);
+
+	_putchar('\n');
+}
